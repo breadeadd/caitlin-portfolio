7ff8973b318d60dfb25f2114f0451f4062b7eb50
Caitlin <caitlinkuan49@gmail.com>
2025-10-04 01:12:09 +1300

    Typed out text working

--- my-portfolio/index.js
+++ my-portfolio/index.js
@@ -3,6 +3,27 @@ const robogalsbtn = document.querySelector("#robogalsBtn");
 const title = document.querySelector("#title");
 const description = document.querySelector("#description");
 
+//typing text
+const div = document.querySelector("#hello");
+const helloText = "Hello, I am Caitlin!";
+
+function textTypingEffect(element, text, i = 0) {
+  if (i === 0) {
+    element.textContent = "";
+  }
+
+  element.textContent += text[i];
+
+  if (i === text.length - 1) {
+    return;
+  }
+
+  setTimeout(() => textTypingEffect(element, text, i + 1), 50);
+}
+
+textTypingEffect(div, helloText);
+
+//buttons for changing involvement text
 sesabtn.addEventListener("click", () => {
   title.textContent = "SESA | MARKETING COORDINATOR";
   description.textContent = "Software Engineering Students' Association (SESA) aims to enhance the experience of software engineering students at UoA. As the Marketing Coordinator, I design graphics and promotional materials for events for students to connect, learn, and have fun.During my time in this role, I have established a iconic branding and mascot design for SESA.";
